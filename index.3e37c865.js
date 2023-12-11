150f32838749b459eedac6515249669eb543ae0a
Mykola Plekan <zikzar@gmail.com>
2023-12-11 21:44:08 +0200

    make build

---
+++ index.3e37c865.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("myForm"),n=document.querySelector('input[name="name"]'),t=document.querySelector('input[name="email"]'),a=document.querySelector('textarea[name="message"]');e.addEventListener("submit",function(e){e.preventDefault(),n.value="",t.value="",a.value=""}),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});
+//# sourceMappingURL=index.3e37c865.js.map
